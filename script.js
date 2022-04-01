535eccb73c3d09d9aba8822f4224ee063e282324
lsrw97 <eugen.el@yahoo.com>
2022-04-01 14:24:05 +0200

    a bit refactored version

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const colors = document.getElementById("colors")
 const main = document.getElementById("mainColor")
 const section = document.getElementById("hex-values")
-let html = ''
 
 function getColorsHtml(){
     let color = document.getElementById("scheme-input").value.substring(1)
     let mode = document.getElementById("scheme-select").value
+
+    //  starting without any html inside main and section
+
     main.innerHTML = ``
     section.innerHTML = ``
+
+    // fetching Data from API. We need to paste a querystring after the endpoint 
+
     fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
         .then(res => res.json())
         .then(data => {
-            console.log(color)
-            console.log(data)
-            console.log(data.count)
-            console.log(data.colors[0].hex.value)
+
+            // creating HTML for the main tag
+
             for(let i = 0; i < data.count; i++){
                 const div = document.createElement('div')
                 div.classList.add('color')
-                console.log(div)
                 div.style.background = data.colors[i].hex.value
                 main.appendChild(div)
             }
+
+            // creating HTML for the section tag
+
             for(let i = 0; i < data.count; i++)
             {
                 section.innerHTML += 
@@ -30,18 +36,8 @@ function getColorsHtml(){
                 `
             }
         })
-    const div = document.createElement('div')
-    div.classList.add('color')
-    div.style.background = ''
 }
 
-document.getElementById("submit-btn").addEventListener('click', function(){
-    getColorsHtml()
-})
+    // adding an eventlistener to the button for excecution of getColorHtml()
 
-//problem is that the hex value is #455231 but I need it without the # for the Api
-
-function hex_to_string(str1)
- {
-
- }
+document.getElementById("submit-btn").addEventListener('click', getColorsHtml)
